81fcbeb764928712de02ef8f675251b66fa18492
andrefrancisco <asfrancisco@ua.pt>
2024-05-20 20:26:09 +0100

    path / com informação do utilizador à entrada deste

--- react-sara/src/components/DefaultLayout.jsx
+++ react-sara/src/components/DefaultLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet, Navigate, useLocation } from "react-router-dom";
 import { useStateContext } from "../contexts/contextprovider";
 import BottomNavBar from "./bottomnavbar";
 import Header from "./header";
@@ -9,6 +9,7 @@ import axiosClient from "../axiosClient";
 export default function DefaultLayout() {
 
     const {id_utilizador, user, token, setUser, setId_utilizador } = useStateContext();
+    const location = useLocation();
 
     useEffect(() => {
         // Verifique se id_utilizador está disponível
@@ -41,8 +42,18 @@ export default function DefaultLayout() {
             <div id="defaultLayout">
                 <div>
                     <Header />
-                    <main className="p-4">
-                    
+                    <main className="p-4 ">
+                    {location.pathname === '/' && (
+                            <>
+                                <h1 className="mt-4 text-green-600 fw-bolder">
+                                    {user.nome_utilizador},
+                                </h1>
+                                <h1 className='indent-3'>bem vindo de novo!!!</h1>
+                                <p>O teu id é o <span className='fw-bolder'>{user.id_utilizador}</span></p>
+                                <p>O teu mail associado é <span className='fw-bolder'>{user.email_utilizador}</span></p>
+                                <p>O teu numero mecanografico é <span className='fw-bolder'>{user.numero_mecanografico_utilizador}</span></p>
+                            </>
+                        )}
 
                         <Outlet />
             
