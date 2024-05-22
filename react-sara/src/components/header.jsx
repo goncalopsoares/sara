5964f365a7e05af1d13d320d7cd7685c399df67c
pedro00chompz <127142259+pedro00chompz@users.noreply.github.com>
2024-05-22 10:22:27 +0100

    NavBar Top updated

--- react-sara/src/components/header.jsx
+++ react-sara/src/components/header.jsx
@@ -1,13 +1,13 @@
 
 import React from 'react';
 import logo from '../images_logo/logo.svg';
-import { FaCartShopping } from "react-icons/fa6";
-import { RiLogoutCircleLine } from "react-icons/ri";
 import axiosClient from '../axiosClient';
 import { useStateContext } from '../contexts/contextprovider';
 import { redirect } from 'react-router-dom';
+import { ShoppingCart } from 'react-feather';
 
-export default function Header() {  
+
+export default function Header() {
 
 const { setUser, setToken, setId_utilizador} = useStateContext();
 
@@ -30,13 +30,40 @@ const { setUser, setToken, setId_utilizador} = useStateContext();
 
     return(
 
-    <header className="bg-green-400 p-4 text-white flex justify-between">
-    <div><img className='img-fluid size-10 ' src={logo} alt="logotipo sara" /></div>
-    <div>
-        <a href="" onClick={onLogout}>  <RiLogoutCircleLine  size={24} /></a>
-        <a  href=""><FaCartShopping size={24} /></a>
+        <div
+            className="p-2 text-white flex justify-between"
+            style={{
+                background: 'linear-gradient(180deg, #FFF 50%, rgba(255, 255, 255, 0.90) 100%)',
+                backdropFilter: 'blur(2px)',
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                width: '100%',
+                zIndex: 1000,
+            }}
+        >
+            <div className="d-flex justify-content-center" style={{padding:"0.5rem"}}>
+                <img src={logo} style={{ width: "2rem", height: "auto"}} />
+            </div>
+            <div style={{ fontSize: '0.5rem', textAlign: 'center', padding:"0.5rem" }}
+                 className="flex flex-col items-center txt-grey-500"
+            >
+                <ShoppingCart style={{ marginBottom: '0.2rem' }} />
+                <div>Carrinho</div>
+            </div>
+
+
+{/*        <div>
+            <img className='img-fluid size-10 ' src={logo} alt="logotipo sara" />
+        </div>
+        <div>
+            <a href="" onClick={onLogout}>  <RiLogoutCircleLine  size={24} /></a>
+            <a  href=""><FaCartShopping size={24} /></a>
+        </div>*/}
+
     </div>
-</header>)
+
+    )
 
 
 }
