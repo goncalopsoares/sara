d04a25f4ffeb3885455006c803b1f02162c5ec4c
Gabriel <hello@gabrielrolino.com>
2024-05-31 12:04:50 +0100

    update of styling on reqDetail

--- react-sara/src/components/requisicao/DetalhesRequisicao.jsx
+++ react-sara/src/components/requisicao/DetalhesRequisicao.jsx
@@ -1,84 +1,168 @@
 export default function DetalhesRequisicao({ detalhesRequisicao }) {
-
     console.log(detalhesRequisicao);
 
     const formatDate = (date) => {
         const options = {
-            weekday: 'long',
-            day: 'numeric',
-            month: 'long',
-            year: 'numeric',
-            hour: 'numeric',
-            minute: '2-digit',
-            hour12: false
+            weekday: "long",
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+            hour: "numeric",
+            minute: "2-digit",
+            hour12: false,
         };
 
-        const dateString = new Date(date).toLocaleDateString('pt-PT', options);
-        const [weekday, dateTime] = dateString.split(',');
+        const dateString = new Date(date).toLocaleDateString("pt-PT", options);
+        const [weekday, dateTime] = dateString.split(",");
 
         return `${dateTime.trim()}, ${weekday}`;
     };
 
-    const sortedUsers = [...detalhesRequisicao.utilizador].sort((a, b) => a.role_utilizador - b.role_utilizador);
-
+    const sortedUsers = [...detalhesRequisicao.utilizador].sort(
+        (a, b) => a.role_utilizador - b.role_utilizador
+    );
 
     return (
         <>
-            <div className='row'>
-                <h3 class='mobile-subtitle'>Contexto da Requisição</h3>
-                <p>{detalhesRequisicao.contexto_requisicao}</p>
-            </div>
-            <div className='row my-4'>
-                <div className='col-6'>
-                    <h3 class='mobile-subtitle'>Data de Recolha</h3>
-                    <p>{formatDate(detalhesRequisicao.data_inicio_requisicao)}</p>
+            <div className="row">
+                <div className="row d-none d-md-flex">
+                    <h3 class="mobile-subtitle mt-4">Contexto da Requisição</h3>
+                    <p>{detalhesRequisicao.contexto_requisicao}</p>
                 </div>
-                <div className='col-6'>
-                    <h3 class='mobile-subtitle'>Data de Devolução</h3>
-                    <p>{formatDate(detalhesRequisicao.data_fim_requisicao)}</p>
-                </div>
-            </div>
-            <div className='row my-4'>
-                <h3 class='mobile-subtitle'>Elementos do Grupo</h3>
-                <div className="flex flex-wrap mt-3">
-                    {sortedUsers.map((user, index) => (
-                        (user.role_utilizador === 3 || user.role_utilizador === 4) && (
-                            <div key={index} className="flex items-center mr-4 mb-4">
-                                <div className="flex flex-col items-center mr-1">
-                                    {user.role_utilizador === 3 && <p className="text-white mb-1 background-green-500 rounded-full p-1" style={{fontSize: '0.75rem'}}>Responsável</p>}
-                                    <img className="rounded-full h-12 w-12" src={`http://localhost:8000${user.avatar_utilizador}`} alt={user.nome_utilizador} />
-                                </div>
-                                <div className="ml-3">
-                                    <p className="font-semibold mb-0">{user.nome_utilizador}</p>
-                                    <p className="text-green-900 mb-0">{user.email_utilizador}</p>
-                                </div>
-                            </div>
-                        )
-                    ))}
-                </div>
-            </div>
-            <div className='row my-4'>
-                <div className='col-6'>
-                    <h3 class='mobile-subtitle'>Unidade Curricular</h3>
-                    <div className="flex items-center mt-3">
-                        <img className="rounded-md h-12 w-12 mr-3" src={`http://localhost:8000${detalhesRequisicao.icone_uc_contexto}`} alt={detalhesRequisicao.nome_uc_contexto} />
-                        <p className="mb-0">{detalhesRequisicao.nome_uc_contexto}</p>
+                <div className="col-12 col-md-6 d-block d-md-none">
+                    <div className="form-group" style={{ marginTop: "1rem" }}>
+                        <div
+                            style={{
+                                display: "flex",
+                                justifyContent: "space-between",
+                                alignItems: "center",
+                            }}
+                        >
+                            <label
+                                style={{
+                                    fontSize: "0.8rem",
+                                    marginBottom: "0.5rem",
+                                }}
+                            >
+                                Contexto da Requisição{" "}
+                            </label>
+                        </div>
+                        <textarea
+                            name="contexto_requisicao"
+                            value={detalhesRequisicao.contexto_requisicao}
+                            readOnly
+                            maxLength="140"
+                            rows="4"
+                            className="form-control rounded w-100"
+                            style={{
+                                fontSize: "0.8rem",
+                                backgroundColor: "#FAFAFA",
+                                color: "black",
+                                marginBottom: "0.5rem",
+                                fontWeight: "200",
+                            }}
+                        />
                     </div>
                 </div>
-                <div className='col-6'>
-                    <h3 class='mobile-subtitle'>Validado pelo Professor</h3>
-                    {detalhesRequisicao.utilizador.map((user, index) => (
-                        (user.role_utilizador === 2) && (
-                            <div key={index} className="flex items-center mr-4 mb-4 mt-3">
-                                <div className="flex items-center">
-                                    <img className="rounded-full h-12 w-12 mr-1" src={`http://localhost:8000${user.avatar_utilizador}`} alt={user.nome_utilizador} />
-                                </div>
-                                <div className='mr-5'>
-                                    <p className="font-semibold mb-0">{user.nome_utilizador}</p>
-                                    <p className="text-green-900 mb-0">{user.email_utilizador}</p>
-                                </div>
-                            </div>)
-                    ))}
+                <div className="row my-4">
+                    <div className="col-6">
+                        <h3 class="mobile-subtitle">Data de Recolha</h3>
+                        <p>
+                            {formatDate(
+                                detalhesRequisicao.data_inicio_requisicao
+                            )}
+                        </p>
+                    </div>
+                    <div className="col-6">
+                        <h3 class="mobile-subtitle">Data de Devolução</h3>
+                        <p>
+                            {formatDate(detalhesRequisicao.data_fim_requisicao)}
+                        </p>
+                    </div>
+                </div>
+                <div className="row my-4">
+                    <h3 class="mobile-subtitle">Elementos do Grupo</h3>
+                    <div className="flex flex-wrap mt-3">
+                        {sortedUsers.map(
+                            (user, index) =>
+                                (user.role_utilizador === 3 ||
+                                    user.role_utilizador === 4) && (
+                                    <div
+                                        key={index}
+                                        className="flex items-center mr-4 mb-4"
+                                    >
+                                        <div className="flex flex-col items-center mr-1">
+                                            {user.role_utilizador === 3 && (
+                                                <p
+                                                    className="text-white mb-1 background-green-500 rounded-full p-1"
+                                                    style={{
+                                                        fontSize: "0.75rem",
+                                                    }}
+                                                >
+                                                    Responsável
+                                                </p>
+                                            )}
+                                            <img
+                                                className="rounded-full h-12 w-12"
+                                                src={`http://localhost:8000${user.avatar_utilizador}`}
+                                                alt={user.nome_utilizador}
+                                            />
+                                        </div>
+                                        <div className="ml-3">
+                                            <p className="font-semibold mb-0">
+                                                {user.nome_utilizador}
+                                            </p>
+                                            <p className="text-green-900 mb-0">
+                                                {user.email_utilizador}
+                                            </p>
+                                        </div>
+                                    </div>
+                                )
+                        )}
+                    </div>
+                </div>
+                <div className="row my-4">
+                    <div className="col-6">
+                        <h3 class="mobile-subtitle">Unidade Curricular</h3>
+                        <div className="flex items-center mt-3">
+                            <img
+                                className="rounded-md h-12 w-12 mr-3"
+                                src={`http://localhost:8000${detalhesRequisicao.icone_uc_contexto}`}
+                                alt={detalhesRequisicao.nome_uc_contexto}
+                            />
+                            <p className="mb-0">
+                                {detalhesRequisicao.nome_uc_contexto}
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <h3 class="mobile-subtitle">Validado pelo Professor</h3>
+                        {detalhesRequisicao.utilizador.map(
+                            (user, index) =>
+                                user.role_utilizador === 2 && (
+                                    <div
+                                        key={index}
+                                        className="flex items-center mr-4 mb-4 mt-3"
+                                    >
+                                        <div className="flex items-center">
+                                            <img
+                                                className="rounded-full h-12 w-12 mr-1"
+                                                src={`http://localhost:8000${user.avatar_utilizador}`}
+                                                alt={user.nome_utilizador}
+                                            />
+                                        </div>
+                                        <div className="mr-5">
+                                            <p className="font-semibold mb-0">
+                                                {user.nome_utilizador}
+                                            </p>
+                                            <p className="text-green-900 mb-0">
+                                                {user.email_utilizador}
+                                            </p>
+                                        </div>
+                                    </div>
+                                )
+                        )}
+                    </div>
                 </div>
             </div>
         </>
--- react-sara/src/views/sara/ReqDetail.jsx
+++ react-sara/src/views/sara/ReqDetail.jsx
@@ -1,31 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import axiosClient from '../../axiosClient';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import axiosClient from "../../axiosClient";
+import { useParams } from "react-router-dom";
 import { useStateContext } from "../../contexts/contextprovider";
-import EstadosMap from '../../components/requisicao/EstadosMap';
-import DetalhesRequisicao from '../../components/requisicao/DetalhesRequisicao';
-import Comentarios from '../../components/requisicao/Comentarios';
-import Equipamentos from '../../components/requisicao/Equipamentos';
+import EstadosMap from "../../components/requisicao/EstadosMap";
+import DetalhesRequisicao from "../../components/requisicao/DetalhesRequisicao";
+import Comentarios from "../../components/requisicao/Comentarios";
+import Equipamentos from "../../components/requisicao/Equipamentos";
+import { useNavigate } from "react-router-dom";
+import { ArrowLeft } from "react-feather";
 
 export default function ReqDetail() {
-
     const { id } = useParams();
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [detalhesRequisicao, setDetalhesRequisicao] = useState(null);
     const [estadoData, setEstadoData] = useState(null);
-    const [comentarioSaraData, setComentarioSaraData] = useState('');
+    const [comentarioSaraData, setComentarioSaraData] = useState("");
     const { user } = useStateContext();
+    const navigate = useNavigate();
 
+    const goBack = () => {
+        navigate(-1);
+    };
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axiosClient.get(`/sara/requisicao/${id}`);
+                const response = await axiosClient.get(
+                    `/sara/requisicao/${id}`
+                );
                 setDetalhesRequisicao(response.data[0]);
                 setLoading(false);
             } catch (error) {
-                console.error('Erro ao obter detalhes da requisição:', error);
+                console.error("Erro ao obter detalhes da requisição:", error);
                 setError(error);
                 setLoading(false);
             }
@@ -35,39 +42,51 @@ export default function ReqDetail() {
 
     const aprovarRejeitar = async (id, estadoData) => {
         try {
-            const response = await axiosClient.post(`/sara/atualizarestado/${id}`, estadoData);
-            console.log('Estado atualizado com sucesso:', response.data);
+            const response = await axiosClient.post(
+                `/sara/atualizarestado/${id}`,
+                estadoData
+            );
+            console.log("Estado atualizado com sucesso:", response.data);
             setEstadoData(null);
-            setComentarioSaraData('');
+            setComentarioSaraData("");
         } catch (error) {
-            console.error('Erro ao atualizar estado:', error.response ? error.response.data : error.message);
+            console.error(
+                "Erro ao atualizar estado:",
+                error.response ? error.response.data : error.message
+            );
         }
     };
 
     const comentar = async (id, comentarioSaraData) => {
         try {
-            const response = await axiosClient.post(`/sara/comentar/${id}`, comentarioSaraData);
-            console.log('Comentário atualizado com sucesso:', response.data);
-            setComentarioSaraData('');
+            const response = await axiosClient.post(
+                `/sara/comentar/${id}`,
+                comentarioSaraData
+            );
+            console.log("Comentário atualizado com sucesso:", response.data);
+            setComentarioSaraData("");
         } catch (error) {
-            console.error('Erro ao atualizar comentário:', error.response ? error.response.data : error.message);
+            console.error(
+                "Erro ao atualizar comentário:",
+                error.response ? error.response.data : error.message
+            );
         }
-    }
+    };
 
     const formatDatePost = (date) => {
         const d = new Date(date);
         const year = d.getFullYear();
-        const month = String(d.getMonth() + 1).padStart(2, '0');
-        const day = String(d.getDate()).padStart(2, '0');
-        const hours = String(d.getHours()).padStart(2, '0');
-        const minutes = String(d.getMinutes()).padStart(2, '0');
-        const seconds = String(d.getSeconds()).padStart(2, '0');
+        const month = String(d.getMonth() + 1).padStart(2, "0");
+        const day = String(d.getDate()).padStart(2, "0");
+        const hours = String(d.getHours()).padStart(2, "0");
+        const minutes = String(d.getMinutes()).padStart(2, "0");
+        const seconds = String(d.getSeconds()).padStart(2, "0");
         return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
     };
 
     const handleClick = (event) => {
         const buttonId = event.target.id;
-        const estadoId = buttonId === 'button1' ? 4 : 6;
+        const estadoId = buttonId === "button1" ? 4 : 6;
 
         const data = {
             requisicao_id_requisicao: id,
@@ -75,10 +94,10 @@ export default function ReqDetail() {
             data_estado: formatDatePost(new Date()),
         };
 
-        console.log('data:', data);
+        console.log("data:", data);
         setEstadoData(data);
         aprovarRejeitar(id, data);
-        console.log('estadoData:', data);
+        console.log("estadoData:", data);
     };
 
     const handleComment = (newComment) => {
@@ -89,55 +108,105 @@ export default function ReqDetail() {
 
         setComentarioSaraData(data);
         comentar(id, data);
-
     };
 
-    console.log('user', user)
+    console.log("user", user);
 
     if (loading) return <div>Loading...</div>;
     if (error) return <div>Error: {error}</div>;
 
     return (
         <>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-8 col-sm-7'>
-                        <h1>Requisicao {detalhesRequisicao.id_requisicao}: {detalhesRequisicao.nome_requisicao}</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="d-flex align-items-center">
+                        <button onClick={goBack} className="mr-2">
+                            <ArrowLeft />
+                        </button>
+                        <div
+                            className="mobile-title truncate"
+                            title={detalhesRequisicao.nome_requisicao}
+                        >
+                            {detalhesRequisicao.nome_requisicao}
+                        </div>
                     </div>
                     <div
-                        className={`col-4 col-sm-5 ${user.tipo_utilizador === 2 && detalhesRequisicao.id_estado === 1 ? 'position-fixed bottom-0' : ''}`}
-                        style={user.tipo_utilizador === 2 && detalhesRequisicao.id_estado === 1 ? { marginBottom: '6rem' } : {}}
+                        className={`col-4 col-sm-5 ${
+                            user.tipo_utilizador === 2 &&
+                            detalhesRequisicao.id_estado === 1
+                                ? "position-fixed bottom-0"
+                                : ""
+                        }`}
+                        style={
+                            user.tipo_utilizador === 2 &&
+                            detalhesRequisicao.id_estado === 1
+                                ? { marginBottom: "6rem" }
+                                : {}
+                        }
                     >
-                        {user.tipo_utilizador === 1 && detalhesRequisicao.id_estado === 3 && (
-                            <div className='flex justify-end'>
-                                <button id='button1' onClick={handleClick} className='btn btn-success mr-5'>Aprovar</button>
-                                <button id='button2' onClick={handleClick} className='btn btn-danger'>Rejeitar</button>
-                            </div>)}
+                        {user.tipo_utilizador === 1 &&
+                            detalhesRequisicao.id_estado === 3 && (
+                                <div className="flex justify-end">
+                                    <button
+                                        id="button1"
+                                        onClick={handleClick}
+                                        className="btn btn-success mr-5"
+                                    >
+                                        Aprovar
+                                    </button>
+                                    <button
+                                        id="button2"
+                                        onClick={handleClick}
+                                        className="btn btn-danger"
+                                    >
+                                        Rejeitar
+                                    </button>
+                                </div>
+                            )}
                     </div>
                 </div>
             </div>
             <div className="container-fluid">
-                <div className="row">
-                    <div className="col-7">
-                        <div className="flex justify-center mb-5">
-                            <EstadosMap detalhesRequisicaoEstado={detalhesRequisicao.id_estado} />
-                        </div>
-                        <div>
-                            <DetalhesRequisicao detalhesRequisicao={detalhesRequisicao} />
-                        </div>
-                        <div className='row my-4'>
-                            <h3 class='mobile-subtitle'>Comentários</h3>
-                            <Comentarios comentarioProfessorRequisicao={detalhesRequisicao.comentario_professor_requisicao} comentarioSaraRequisicao={detalhesRequisicao.comentario_sara_requisicao} utilizadores={detalhesRequisicao.utilizador} onSubmitComment={handleComment} currentUser={user} />
-                        </div>
-
-                    </div>
-                    <div className='col-5'>
-                        <h3>Equipamentos</h3>
-                        <Equipamentos listaEquipamentos={detalhesRequisicao.equipamento} />
-
-                    </div>
-                </div>
+    <div className="row">
+        <div className="col-sm-7">
+            <div className="flex justify-center mb-5 w-100 w-sm-auto">
+                <EstadosMap
+                    detalhesRequisicaoEstado={
+                        detalhesRequisicao.id_estado
+                    }
+                />
             </div>
+            <div className="mobile-subtitle mt-sm-3">
+                Resumo da requisição #{detalhesRequisicao.id_requisicao}
+            </div>
+            <div>
+                <DetalhesRequisicao
+                    detalhesRequisicao={detalhesRequisicao}
+                />
+            </div>
+            <div className="row my-4">
+                <h3 class="mobile-subtitle">Comentários</h3>
+                <Comentarios
+                    comentarioProfessorRequisicao={
+                        detalhesRequisicao.comentario_professor_requisicao
+                    }
+                    comentarioSaraRequisicao={
+                        detalhesRequisicao.comentario_sara_requisicao
+                    }
+                    utilizadores={detalhesRequisicao.utilizador}
+                    onSubmitComment={handleComment}
+                    currentUser={user}
+                />
+            </div>
+        </div>
+        <div className="col-5">
+            <h3>Equipamentos</h3>
+            <Equipamentos
+                listaEquipamentos={detalhesRequisicao.equipamento}
+            />
+        </div>
+    </div>
+</div>
         </>
     );
-};
+}
